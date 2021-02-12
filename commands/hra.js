e8b40c15d85cf20e961411ffffcbda34d0557c94
Tomáš Podolský <podolsky.tomas@gmail.com>
2021-02-12 21:32:48 +0100

    continue in game creation

--- commands/hra.js
+++ commands/hra.js
@@ -3,17 +3,31 @@ module.exports = {
     description: "Kámen nůžky papír :-) ",
     execute(msg) {
         msg.reply("Tak si pojďme zahrát! :smirk: Máš 20 vteřin na reakci, tik tak tik tak 😁").then(sentMsg => {
-            sentMsg.react('🪨'); // rock
-            sentMsg.react('✂️'); // scissors
-            sentMsg.react('🧻'); // paper
-            // TODO - emotikony dát do array, reagovat všemi, po reakci hráče pak vybrat náhodné číslo 1-3 a vyhodnotit výsledek
+            let emojiArray = ['🪨', '✂️', '🧻'];
+
+            emojiArray.forEach(oneEmoji => sentMsg.react(oneEmoji));
+
 
             sentMsg.awaitReactions((reaction, user) => user.id == msg.author.id && (reaction.emoji.name == '🪨' || reaction.emoji.name == '✂️' || reaction.emoji.name == '🧻') ,
-                { max: 1, time: 20000}).then(collected => {
-                    msg.reply(collected.first().emoji.name);
-                }).catch(() => {
-                    msg.reply("Pozdě! Vyhrál jsem! 😁");
-                });
+            { max: 1, time: 20000}).then(collected => {
+
+                let reactedIndex = emojiArray.indexOf(collected.first().emoji.name);
+
+                let myIndex = Math.floor(Math.random() * Math.floor(3));
+
+                // TODO - rozhodnuti o vysledku
+                if (myIndex === 0){
+                    if (reactedIndex === 0) {
+                        msg.reply(`Remiza`);
+                    }
+                }
+
+                msg.reply(`my index is ${myIndex} / reacted index is ${reactedIndex}`);
+                //msg.reply(collected.first().emoji.name);
+
+            }).catch(() => {
+                msg.reply("Pozdě! Vyhrál jsem! 😁");
+            });
         });
     }
 };
