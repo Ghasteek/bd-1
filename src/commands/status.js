9fea0463f834c479fd8b08fdbdb6c1695774152d
Tomáš Podolský <podolsky.tomas@gmail.com>
2021-10-26 21:07:10 +0200

    few fixes and feats
    fix: status message splitting
    feat: status message into whisper with parameter "w"
    fix: styled status message

--- src/commands/status.js
+++ src/commands/status.js
@@ -3,12 +3,20 @@ const os = require('os');
 const axios = require('axios');
 const logHistory = require('../services/logger').history;
 
-
+/**
+ * Takes integer number and add leading zeros
+ * @param {Integer} number - input number
+ * @returns String - String number with leading zeros
+ */
+function pad(number) {
+    if (number < 10) return `0${number}`
+    return `${number}`
+}
 
 module.exports = {
     name: 'status',
     description: 'Bot status',
-    async execute(msg) {
+    async execute(msg, args) {
         if (msg.author.id !== process.env.ADMIN_ID) {
             return msg.reply('Nemáte právo na zobrazení stavu systému.');
         }
@@ -18,30 +26,41 @@ module.exports = {
         const uptime = os.uptime;
 
 
-        var sec_num = parseInt(uptime, 10)
-        var days = Math.floor(sec_num / 86400)
-        var hours = Math.floor((sec_num - (days * 86400)) / 3600)
-        var minutes = Math.floor(sec_num / 60) % 60
+        var sec_num = parseInt(uptime, 10);
+        var days = Math.floor(sec_num / 86400);
+        var hours = Math.floor((sec_num - (days * 86400)) / 3600);
+        var minutes = Math.floor(sec_num / 60) % 60;
         var seconds = sec_num % 60
 
-        const uptimeStr = `${days} dní ${hours}:${minutes}:${seconds}`;
+        const uptimeStr = `${days} dní ${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
 
-        let ip;
+        let ip = '';
         await axios.get('https://api.myip.com')
             .then(res => {
                 ip = res.data.ip;
             });
+        let message = `\n🖥️ Systém:\n` +
+            '\`\`\`' +
+            `freeRAM:  ${freeRam} GB\n` +
+            `maxRAM:   ${maxRam} GB\n` +
+            `uptime:   ${uptimeStr}\n` +
+            `IP:       ${ip}\n` +
+            '\`\`\`\n' +
+            `📝 Log:\n\`\`\`` +
+            logHistory.join('\n') +
+            '\`\`\`';
 
-        msg.reply({
-            embed: {
-                color: '#000000',
-                title: "Status:",
-                fields: [
-                    { name: 'Systém', value: 'free RAM\ntotal RAM\nIP\nUptime', inline: true },
-                    { name: 'Hodnota', value: `${freeRam}GB\n${maxRam}GB\n${ip}\n${uptimeStr}`, inline: true },
-                    { name: 'LOG', value: `${logHistory.join('\n')}`, inline: false },
-                ]
-            }
-        });
+        if (args[0] === 'w') {
+            msg.author.send(
+                message,
+                { split: true }
+            );
+        } else {
+            msg.channel.send(
+                message,
+                { split: true }
+            );
+        }
+        msg.delete();
     },
 };
